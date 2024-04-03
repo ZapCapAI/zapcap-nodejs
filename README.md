cb14459830ba14c92aa084209e72ab07dac375da
zapcap dev <dev@zapcap.dev>
2024-04-03 21:50:10 +1100

    add zapcap-client

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# zapcap-nodejs
+# 🚀 ZapCap SDK
+
+The ZapCap SDK provides a powerful interface to the ZapCap API, allowing you to effortlessly generate amazing subtitles for your videos, manage video tasks, and streamline your video processing workflow with just a few lines of code.
+
+## 📦 Installation
+
+```bash
+npm install zapcap
+```
+
+or
+
+```
+yarn add zapcap
+```
+
+## 🔑 Setting Up
+
+Before you start, make sure you have your ZapCap API Key. Configure it in your environment:
+
+```
+ZAPCAP_API_KEY=your_api_key_here
+```
+
+## 🌟 Getting Started
+
+```
+import { ZapCap } from 'zapcap-sdk';
+import fs from 'fs';
+
+(async () => {
+  const zapcap = new ZapCap({
+    apiKey: process.env.ZAPCAP_API_KEY,
+  });
+
+  // Upload a video
+  const videoId = await zapcap.uploadVideo(fs.createReadStream("path/to/your/video.mp4"));
+
+  // Create a video task with the first available template
+  const templates = await zapcap.getTemplates();
+  const templateId = templates[0].id;
+  const taskId = await zapcap.createVideoTask(videoId, templateId, true);
+
+  console.log(`Video uploaded and task created with ID: ${taskId}`);
+})();
+```
+
+## 📚 Features
+
+- 📹 <b>Video Upload:</b> Upload your videos for processing.
+- 🛠 <b>Task Management:</b> Create tasks for videos, poll their status, and fetch results.
+- 📝 <b>Transcription:</b> Approve, get, and update video transcriptions.
+- 🎨 <b>Templates:</b> Utilize templates to standardize video processing.
+- 🧾 <b>Billing Information:</b> Check your usage and remaining render duration.
+
+## 📄 API Reference
+
+Refer to the official [ZapCap API documentation](https://api.zapcap.dev/api) for a comprehensive list of endpoints and their functionalities.
+
+## ✉️ Support
+
+For support, please open an issue or contact hi@zapcap.dev.
+
+---
+
+Happy coding! 🎉
---
+++ src/zapcap.ts
@@ -0,0 +1,231 @@
+import axios, { AxiosInstance, AxiosPromise, AxiosResponse } from "axios";
+import {
+  CreateVideoResponseDto,
+  DefaultApi,
+  GetVideoTaskResponseDto,
+  TemplateDto,
+  VideoTaskStatusEnum,
+} from "./api";
+import FormData from "form-data";
+import { Readable } from "stream";
+import { Configuration } from "./configuration";
+
+const BASE_PATH = "https://api.zapcap.dev";
+
+export type ZapCapOptions = {
+  apiKey: string;
+  axios?: AxiosInstance;
+  basePath?: string;
+};
+
+export type PollForTranscriptOptions = {
+  retryFrequency: number; // in milliseconds
+  timeout: number; // in milliseconds
+};
+
+export type ProcessedWords = {
+  words: ProcessedWord[];
+};
+export type ProcessedWord = {
+  end: number;
+  text: string;
+  type: string;
+  start: number;
+  end_time: number;
+  confidence: number;
+  start_time: number;
+  emoji?: string;
+  important?: boolean;
+  id: string;
+};
+
+export class ZapCap {
+  private _client: DefaultApi;
+  private _axios: AxiosInstance;
+  public helpers: ZapCapHelpers;
+
+  constructor(options: ZapCapOptions) {
+    const basePath = options.basePath ?? BASE_PATH;
+    const apiKey = options.apiKey;
+
+    const config = new Configuration({
+      apiKey,
+      basePath,
+    });
+    this._axios =
+      options.axios ??
+      axios.create({
+        baseURL: basePath,
+        headers: { "User-Agent": "zapcap-sdk:1.0.0", "x-api-key": apiKey },
+      });
+
+    this._client = new DefaultApi(config, BASE_PATH, this._axios);
+    this.helpers = new ZapCapHelpers(this, this._axios);
+  }
+
+  async getTemplates(): AxiosPromise<Array<TemplateDto>> {
+    return await this._client.getTemplates();
+  }
+
+  async getVideoTask(
+    videoId: string,
+    taskId: string
+  ): AxiosPromise<GetVideoTaskResponseDto> {
+    return await this._client.getVideoTask(videoId, taskId);
+  }
+
+  async approveTranscript(
+    videoId: string,
+    taskId: string
+  ): Promise<AxiosPromise<void>> {
+    return await this._client.approveTranscript(videoId, taskId);
+  }
+
+  async uploadVideo(file: File): AxiosPromise<CreateVideoResponseDto>;
+  async uploadVideo(file: Readable): AxiosPromise<CreateVideoResponseDto>;
+  async uploadVideo(...args: any[]): AxiosPromise<CreateVideoResponseDto> {
+    const file = args[0];
+    const formData = new FormData();
+
+    if (args.length === 1 && args[0] instanceof File) {
+      //@ts-ignore
+      formData.append("file", Readable.fromWeb(file.stream()), {
+        filename: file.name,
+        contentType: file.type,
+      });
+    } else if (args.length === 1 && args[0] instanceof Readable) {
+      const form = new FormData();
+      form.append("file", file);
+
+      return await this._axios.post("videos", form);
+    } else {
+      throw new Error("Invalid arguments provided to uploadVideo");
+    }
+
+    return this._axios.post("videos", formData);
+  }
+
+  async createVideoTask(
+    videoId: string,
+    templateId: string,
+    autoApprove = false
+  ) {
+    return await this._client.createTask(videoId, {
+      templateId,
+      autoApprove,
+    });
+  }
+}
+
+class ZapCapHelpers {
+  private _zapCap: ZapCap;
+  private _axios: AxiosInstance;
+
+  constructor(zapCap: ZapCap, axios: AxiosInstance) {
+    this._zapCap = zapCap;
+    this._axios = axios;
+  }
+
+  async pollForTranscript(
+    videoId: string,
+    taskId: string,
+    options: PollForTranscriptOptions = {
+      retryFrequency: 5000,
+      timeout: 60000,
+    },
+    verboseLogging: boolean = true
+  ): Promise<ProcessedWords> {
+    const { retryFrequency, timeout } = options;
+    const startTime = Date.now();
+    let pollCount = 0;
+
+    const checkStatus = async (): Promise<ProcessedWords> => {
+      pollCount++;
+      if (verboseLogging) {
+        console.log(`Transcript polling attempt #${pollCount}`);
+      }
+      const response = await this._zapCap.getVideoTask(videoId, taskId);
+      if (response.data.status === VideoTaskStatusEnum.Failed) {
+        throw new Error("ZapCap: Video task failed");
+      }
+      if (response.data.transcript) {
+        if (verboseLogging) {
+          console.log("Transcript ready for download.");
+        }
+        const transcriptUrl = response.data.transcript;
+        const download = await this._axios.get<ProcessedWords>(transcriptUrl);
+        return download.data;
+      } else if (Date.now() - startTime > timeout) {
+        return Promise.reject(
+          new Error(
+            "ZapCap: Timeout reached while waiting for transcription to complete"
+          )
+        );
+      } else {
+        if (verboseLogging) {
+          console.log(
+            `Transcript not ready yet, retrying in ${retryFrequency}ms...`
+          );
+        }
+        await new Promise((resolve) => setTimeout(resolve, retryFrequency));
+        return checkStatus();
+      }
+    };
+
+    return checkStatus();
+  }
+
+  async pollForRender(
+    videoId: string,
+    taskId: string,
+    options: PollForTranscriptOptions = {
+      retryFrequency: 5000,
+      timeout: 120000,
+    },
+    verboseLogging: boolean = true
+  ): Promise<Readable> {
+    const { retryFrequency, timeout } = options;
+    const startTime = Date.now();
+    let pollCount = 0;
+
+    const checkStatus = async (): Promise<Readable> => {
+      pollCount++;
+      if (verboseLogging) {
+        console.log(`Render polling attempt #${pollCount}`);
+      }
+
+      const response = await this._zapCap.getVideoTask(videoId, taskId);
+      if (!response.data.transcriptApproved) {
+        throw new Error("ZapCap: Transcript it not approved yet");
+      }
+      if (response.data.status === VideoTaskStatusEnum.Failed) {
+        throw new Error("ZapCap: Video task failed");
+      }
+      if (response.data.downloadUrl) {
+        if (verboseLogging) {
+          console.log("Downloading render...");
+        }
+        const downloadUrl = response.data.downloadUrl;
+        const download: AxiosResponse<Readable> = await this._axios.get(
+          downloadUrl,
+          {
+            responseType: "stream",
+          }
+        );
+        return download.data;
+      } else if (Date.now() - startTime > timeout) {
+        throw new Error("Timeout reached while waiting for render to complete");
+      } else {
+        if (verboseLogging) {
+          console.log(
+            `Render not ready yet, retrying in ${retryFrequency}ms...`
+          );
+        }
+        await new Promise((resolve) => setTimeout(resolve, retryFrequency));
+        return checkStatus();
+      }
+    };
+
+    return checkStatus();
+  }
+}
